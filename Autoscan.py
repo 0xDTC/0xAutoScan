6137e9e71acca289171fcaa1806d7c25baa0cd9d
TanveerS1ngh <95960398+TanveerS1ngh@users.noreply.github.com>
2024-03-03 16:34:53 +0530

    updated autoscan.py

--- Autoscan.py
+++ Autoscan.py
@@ -1,10 +1,11 @@
 import os
 
-os.system(if EUID == 0:
-   echo "This script must be run as root" 
-else:  
-   exit 1)
-
-
-
+# Check if the script is run as root
+if os.geteuid() == 0:
+    print("This script is running as root")
+else:
+    print("This script must be run as root")
+    exit(1)
+    
+# Continue with other commands if running as root
 os.system('mkdir user')
