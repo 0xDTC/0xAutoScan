7b92bcc55a247225fbdad812d159e3c7ced72fdb
TanveerS1ngh <95960398+TanveerS1ngh@users.noreply.github.com>
2024-06-18 14:00:56 +0530

    ^M is a character used by Windows to mark the end of a line fix updated the tool

--- Autoscan.sh
+++ Autoscan.sh
@@ -24,43 +24,34 @@ confirmYes() {
 read -p "Enter the directory location: " DIR
 
 # If provided $DIR is not a directory, ask user to create or quit
-if [ ! -d $DIR ]; then
+if [ ! -d "$DIR" ]; then
    echo "Dir not found: $DIR"
-
-   if confirmYes "Created dir now?"; then
-      mkdir -p $DIR
+   if confirmYes "Create dir now?"; then
+      mkdir -p "$DIR"
    else
       echo "Aborting, DIR not set"
       exit 1
    fi
 fi
 
-echo "Dir location you enter to save progress $DIR"
-
+echo "Dir location you entered to save progress: $DIR"
 read -p "Enter the IP: " IP
-
-echo "The IP you are using $IP"
-
+echo "The IP you are using: $IP"
 read -p "Add Dir name if Ctf: " Ctf
-
-cd $DIR
-
+cd "$DIR"
 mkdir Results
-
 cd Results
 
 # If you want Ctf box name Directory otherwise Make Ip name Directory
-
 if [[ "$Ctf" != "" ]]; then
-   mkdir $Ctf
-   cd $Ctf
+   mkdir "$Ctf"
+   cd "$Ctf"
 else
-   mkdir $IP
-   cd $IP
+   mkdir "$IP"
+   cd "$IP"
 fi
 
 # Tools list you can add in the one you use mostly or use same
-
 echo -e "Select multiple tools for scan
 
    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -68,7 +59,7 @@ echo -e "Select multiple tools for scan
    #  2. Dirsearch                     #
    #  3. Nikto                         #
    #  4. Amass                         #
-   #  5. Sublister                     #
+   #  5. subfinder                     #
    #  6. Gobuster                      #
    #  7. Nuclei                        #
    #  8. Wpscan                        #
@@ -81,27 +72,25 @@ IFS=, read -p "Selected tools (comma separated): " -a tools
 
 for tool in "${tools[@]}"; do
    case $tool in
-      1) sudo nmap -A -sC -sV -p- -v $IP -oN nmap ;;
-      2) sudo dirsearch -u $IP -q -r -e php,js,db,xml,html -i 200-299,300-399,500-599 -format simple -o dirsearch;;
-      3) nikto -url $IP -no404 -C all -Format txt -o nikto;;
-      4) amass enum -d $IP.com > amass;;
-      5) sublist3r -d $IP -o sublister;;
-      6) gobuster dir $IP -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt -q -t 3 -r -x php,js,db,xml,html -o $IP_"gobuster;;
-      7) nuclei -u $IP;;
-      8) wpscan --url $IP;;
-
+      1) sudo nmap -A -sC -sV -p- -v "$IP" -oN nmap ;;
+      2) sudo dirsearch -u "$IP" -q -r -e php,js,db,xml,html -i 200-299,300-399,500-599 -format simple -o dirsearch;;
+      3) nikto -url "$IP" -no404 -C all -Format txt -o nikto;;
+      4) amass enum -d "$IP".com > amass;;
+      5) subfinder -d "$IP" -o subfinder;;
+      6) gobuster dir -u "$IP" -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt -q -t 3 -r -x php,js,db,xml,html -o "$IP"_gobuster;;
+      7) nuclei -u "$IP";;
+      8) wpscan --url "$IP";;
+      9) hamster -u "$IP";; # Assuming you meant Hamster tool
+      10) sqlmap -u "$IP";;
       *)
-
-      echo "invalid option script restarting"
+      echo "Invalid option, script restarting"
       clear
-      sleep 3;;
+      sleep 3
+      exec "$0" # Restart the script
+      ;;
    esac
 done
 
-
 # remove history
-
 rm -rf /home/clown/.zsh_history
-
 rm -rf /home/clown/.bash_history
-
